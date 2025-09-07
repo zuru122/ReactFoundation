2e6aa364593f7efaf431708feae78748dc9448e1
zuru <onyekaozuru122@gmail.com>
2025-09-07 23:25:31 +0100

    Pizza app

---
+++ 03-pizza-menu/src/App.jsx
@@ -0,0 +1,76 @@
+// import reactLogo from './assets/react.svg'
+
+import './App.css'
+
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+
+
+function App() {
+
+  return (
+    <>
+      <div className='header'>
+
+      <h1>React!</h1>
+
+      </div>
+      <Pizza />
+    </>
+  )
+}
+
+function Pizza() {
+  return (
+    <>
+    <h2>Pizza Spinaci</h2>
+    
+    </>
+  )
+}
+
+export default App
